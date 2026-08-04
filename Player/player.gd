b68143b59aeb26f260cf8e429f9341a64554367b
Omarfadalla <omarfadalla@gmail.com>
2026-08-05 00:43:54 +0300

    added my house model i made using blender
    whew! that was a lot of hassle

--- Player/player.gd
+++ Player/player.gd
@@ -6,16 +6,14 @@ const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
