efad89508a42fe6b95867025e1b0c29630ea6ab1
Omarfadalla <omarfadalla@gmail.com>
2026-08-07 23:49:49 +0300

    added dynamic crosshair

--- Player/player_interact.gd
+++ Player/player_interact.gd
@@ -1,12 +1,22 @@
 extends RayCast3D
+
+@onready var crosshair = get_parent().get_parent().get_node("player_ui")
+
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
 		if hit.name == "door":
+			if !crosshair.visible:
+				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().get_parent().get_parent().toggle_door()
 	if is_colliding():
 		var hit = get_collider()
 		if hit.name == "light_switch":
+			if !crosshair.visible:
+				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
-				hit.get_parent().toggle_lights()
+				hit.get_parent().toggle_lights() 
+	else:
+		if crosshair.visible:
+				crosshair.visible = false
