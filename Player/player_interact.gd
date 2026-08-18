a406ac8962902a6b4b5c62c612f8b0874a635cd2
Omarfadalla <omarfadalla@gmail.com>
2026-08-19 00:52:43 +0300

    finalised key opening of the upper door and set up navigation agent

--- Player/player_interact.gd
+++ Player/player_interact.gd
@@ -2,6 +2,7 @@ extends RayCast3D
 
 @onready var player_ui = get_tree().current_scene.get_node("player/player_ui")
 @onready var crosshair = player_ui.get_node("crosshair")
+@export var key_hold : Node3D
 
 var carried_key = null
 
@@ -17,7 +18,7 @@ func _physics_process(delta: float) -> void:
 			if not crosshair.visible:
 				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
-				hit.hit_obj(self)
+				hit.hit_obj(key_hold)
 				carried_key = hit
 		elif hit.name == "door":
 			if not crosshair.visible:
--- scenery/key.gd
+++ scenery/key.gd
@@ -5,12 +5,26 @@ extends RigidBody3D
 @export var is_pickup : bool = true
 
 @onready var rng = RandomNumberGenerator.new()
+@onready var mesh = $key
+@onready var collision = $CollisionShape3D
+
 var pos_obj
+
+const WORLD_SCALE := 30.0
+const WORLD_ROT_Y := 0.0
+const HELD_SCALE := 2.0
+const HELD_ROT_Y := -90.0
+
+
+const MODEL_FIX_Y := 180.0
 
 func _ready() -> void:
 	add_to_group("key")
 	var chance = rng.randi_range(0, positions.size() - 1)
 	global_transform.origin = positions[chance].global_transform.origin
+	rotation = Vector3(rotation.x, deg_to_rad(WORLD_ROT_Y), rotation.z)
+	mesh.scale = Vector3.ONE * WORLD_SCALE
+	collision.scale = Vector3.ONE * WORLD_SCALE
 	freeze = true
 	if not is_pickup:
 		visible = false
@@ -19,6 +33,8 @@ func _ready() -> void:
 func hit_obj(body):
 	pos_obj = body
 	freeze = true
+	mesh.scale = Vector3.ONE * HELD_SCALE
+	collision.scale = Vector3.ONE * HELD_SCALE
 	for child in get_children():
 		if child is CollisionShape3D:
 			child.disabled = true
@@ -26,6 +42,8 @@ func hit_obj(body):
 func insert_into_door():
 	pos_obj = null
 	set_physics_process(false)
+	mesh.scale = Vector3.ONE * 5.0
+	collision.scale = Vector3.ONE * 5.0
 	for shape in get_children():
 		if shape is CollisionShape3D:
 			shape.disabled = true
@@ -36,28 +54,52 @@ func insert_into_door():
 		return
 
 	var target = door.keyhole
-	var insert_offset = target.global_transform.basis.z * 0.15  # start slightly outside the hole
+	var target_basis = target.global_transform.basis.orthonormalized()
+	var target_quat = target_basis.get_rotation_quaternion()
+
+	var insert_offset = (target_basis.z * -0.15) + (target_basis.x * 0.15)
+
+	var start_quat = global_transform.basis.orthonormalized().get_rotation_quaternion()
+	var start_pos = global_position
 
 	var tween = create_tween()
-	# Move + rotate into position just outside the keyhole
+
 	tween.set_parallel(true)
-	tween.tween_property(self, "global_position", target.global_position + insert_offset, 0.35)
-	tween.tween_property(self, "global_rotation", target.global_rotation, 0.35)
+	tween.tween_property(self, "global_position", target.global_position + insert_offset, 1.1)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_method(func(t):
+		var q = start_quat.slerp(target_quat, t)
+		global_transform.basis = Basis(q)
+	, 0.0, 1.0, 1.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	tween.set_parallel(false)
 
-	# Slide into the hole
-	tween.tween_property(self, "global_position", target.global_position, 0.25)
+	tween.tween_interval(0.3)
 
-	# Turn like a real lock: rotate forward then back around local Z
-	tween.tween_property(self, "rotation:z", rotation.z + deg_to_rad(35), 0.2)
-	tween.tween_property(self, "rotation:z", rotation.z - deg_to_rad(35), 0.25)
+	tween.tween_property(self, "global_position", target.global_position, 0.9)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_interval(0.25)
+
+	var turn_points = [0.0, deg_to_rad(20), deg_to_rad(35), deg_to_rad(-35)]
+	var turn_durations = [0.55, 0.65, 0.8]
+
+	for i in range(turn_durations.size()):
+		var from_angle = turn_points[i]
+		var to_angle = turn_points[i + 1]
+		tween.tween_method(func(angle):
+			var turn_quat = Quaternion(Vector3(0, 0, 1), angle)
+			global_transform.basis = Basis(target_quat * turn_quat)
+		, from_angle, to_angle, turn_durations[i]).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_interval(0.2)
 
 	tween.tween_callback(func():
 		door.unlock()
 		queue_free()
 	)
-
 func _physics_process(delta: float) -> void:
 	if pos_obj != null:
 		global_transform.origin = pos_obj.global_transform.origin
-		global_transform.basis = global_transform.basis.slerp(pos_obj.global_transform.basis, 0.3)
+		var target_basis = pos_obj.global_transform.basis.orthonormalized()
+		target_basis = target_basis.rotated(target_basis.y, deg_to_rad(HELD_ROT_Y + MODEL_FIX_Y))
+		global_transform.basis = global_transform.basis.orthonormalized().slerp(target_basis, 0.3)
